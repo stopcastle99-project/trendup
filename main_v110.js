33c1cb4063d4f8bcbc4217b60c6abf45ad029bab
stopcastle <stopcastle@stopcastles-Mac-Studio.local>
2026-03-31 16:12:55 +0900

    fix: use absolute paths for scripts to prevent MIME type errors (v3.2.03)

--- main_v110.js
+++ main_v110.js
@@ -357,7 +357,7 @@ class App {
     this.init();
   }
   async init() {
-    console.log("App Init: v3.2.02");
+    console.log("App Init: v3.2.03");
     try {
       this.initThemeIcons();
       this.applyTheme(this.themeMode);
